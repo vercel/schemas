8359d2ccf599fb0de7d679a51f2bb5b8243cdb34
tknickman <tom.knickman@vercel.com>
2022-04-21 10:52:49 -0400

    chore(prettier): format files

--- user/index.js
+++ user/index.js
@@ -2,117 +2,117 @@ const Username = {
 	type: 'string',
 	minLength: 1,
 	maxLength: 48,
-	pattern: '^[a-z0-9][a-z0-9-]*[a-z0-9]$'
+	pattern: '^[a-z0-9][a-z0-9-]*[a-z0-9]$',
 };
 
 const Name = {
 	type: 'string',
 	minLength: 1,
-	maxLength: 32
+	maxLength: 32,
 };
 
 const Email = {
 	type: 'string',
 	minLength: 5,
-	maxLength: 256
+	maxLength: 256,
 };
 
 const ImportFlowGitProvider = {
 	oneOf: [
 		{
-			'enum': ['github', 'gitlab', 'bitbucket']
+			enum: ['github', 'gitlab', 'bitbucket'],
 		},
 		{
-			type: 'null'
-		}
-	]
+			type: 'null',
+		},
+	],
 };
 
 const ImportFlowGitNamespace = {
 	oneOf: [
 		{
-			type: 'string'
+			type: 'string',
 		},
 		{
-			type: 'null'
-		}
-	]
+			type: 'null',
+		},
+	],
 };
 
 const ImportFlowGitNamespaceId = {
 	oneOf: [
 		{
-			type: 'string'
+			type: 'string',
 		},
 		{
-			type: 'number'
+			type: 'number',
 		},
 		{
-			type: 'null'
-		}
-	]
+			type: 'null',
+		},
+	],
 };
 
 const ScopeId = {
-	type: 'string'
+	type: 'string',
 };
 
 const GitNamespaceId = {
 	oneOf: [
 		{
-			type: 'string'
+			type: 'string',
 		},
 		{
-			type: 'number'
+			type: 'number',
 		},
 		{
-			type: 'null'
-		}
-	]
+			type: 'null',
+		},
+	],
 };
 
 const PlatformVersion = {
 	oneOf: [
 		{
 			// A `null` platform version means to always use the latest
-			type: 'null'
+			type: 'null',
 		},
 		{
 			type: 'integer',
 			minimum: 1,
-			maximum: 2
-		}
-	]
+			maximum: 2,
+		},
+	],
 };
 
 const Avatar = {
 	type: 'string',
 	minLength: 40,
 	maxLength: 40,
-	pattern: '^[0-9a-f]+$'
+	pattern: '^[0-9a-f]+$',
 };
 
 const Bio = {
-	type: 'string'
+	type: 'string',
 };
 
 const Website = {
 	type: 'string',
 	minLength: 4,
-	maxLength: 40
+	maxLength: 40,
 };
 
 const Profile = {
 	type: 'object',
 	properties: {
 		service: {
-			type: 'string'
+			type: 'string',
 		},
 		link: {
-			type: 'string'
-		}
+			type: 'string',
+		},
 	},
-	additionalProperties: false
+	additionalProperties: false,
 };
 
 const Profiles = {
@@ -121,18 +121,18 @@ const Profiles = {
 	maxItems: 100,
 	uniqueItems: true,
 	items: Profile,
-	additionalProperties: false
+	additionalProperties: false,
 };
 
 const RemoteCaching = {
 	type: 'object',
 	properties: {
 		enabled: {
-			type: 'boolean'
+			type: 'boolean',
 		},
 	},
-	additionalProperties: false
-}
+	additionalProperties: false,
+};
 
 const User = {
 	type: 'object',
@@ -141,7 +141,7 @@ const User = {
 		username: Username,
 		name: Name,
 		email: Email,
-		billingChecked: {type: 'boolean'},
+		billingChecked: { type: 'boolean' },
 		avatar: Avatar,
 		platformVersion: PlatformVersion,
 		bio: Bio,
@@ -152,8 +152,8 @@ const User = {
 		importFlowGitNamespaceId: ImportFlowGitNamespaceId,
 		scopeId: ScopeId,
 		gitNamespaceId: GitNamespaceId,
-		RemoteCaching: RemoteCaching
-	}
+		remoteCaching: RemoteCaching,
+	},
 };
 
 module.exports = {
@@ -167,5 +167,5 @@ module.exports = {
 	ImportFlowGitNamespace,
 	ImportFlowGitNamespaceId,
 	ScopeId,
-	GitNamespaceId
+	GitNamespaceId,
 };
